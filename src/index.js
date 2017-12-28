7f378df4e6152fb1bade617baae9192d903facc8
github username <paulyarabe@post.harvard.edu>
2017-12-28 01:17:16 -0500

    updated url fetch request to get from production, not development server

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ class App extends Component {
         titles: Object.keys(highlight_hash).sort()
       }))
     .then(() => {
-      fetch('http://localhost:3000/urls')
+      fetch('https://hilight-db.herokuapp.com/urls')
       .then(resp => resp.json())
       .then(urls => this.setState({
         urls: urls
