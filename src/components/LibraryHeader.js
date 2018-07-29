f0956231f272f4ac38e4b5f09a1ebf207cd28ee5
Paul Yarabe <paulyarabe@post.harvard.edu>
2018-07-29 14:05:25 -0400

    spacing before emoji in welcome text again

--- src/components/LibraryHeader.js
+++ src/components/LibraryHeader.js
@@ -18,7 +18,7 @@ const LibraryHeader = () => {
                         <li><a href="https://paulyarabe.com" target="_blank" rel="noopener noreferrer">blog</a></li>
                     </ul>
                 </div>
-              <h4 className="instructions">Welcome :) Click in the green space next to a book title to load my highlights for that book (it’ll take a second). Then, message me about a highlight (each has a unique ID) that interests you, and we can discuss it! Works best on everything but iPhone..ensp✧*。ヾ(｡&gt;﹏&lt;｡)ﾉﾞ✧*。</h4>
+              <h4 className="instructions">Welcome :) Click in the green space next to a book title to load my highlights for that book (it’ll take a second). Then, message me about a highlight (each has a unique ID) that interests you, and we can discuss it! Works best on everything but iPhone..&ensp;✧*。ヾ(｡&gt;﹏&lt;｡)ﾉﾞ✧*。</h4>
             </div>
         </nav>
     </div>
